926666fb03590218db5e8026959cdc49da45465d
Ronaldo Veloso Filho <ronaldovf24@gmail.com>
2025-06-23 23:09:18 -0300

    Account.js & AccountCategory.js changes
    - Requerido agora é falso

--- models/AccountCategory.js
+++ models/AccountCategory.js
@@ -4,7 +4,7 @@ const AccountCategorySchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true
+    required: false
   },
   category: {
     type: mongoose.Schema.Types.ObjectId,
